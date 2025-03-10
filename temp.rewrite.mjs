39b59931f3fd1d080dd429716f06a2c56793124c
Paul Tarvydas <paul.tarvydas@gmail.com>
2025-03-10 08:52:00 -0400

    5.json - maybe enough culling?

--- temp.rewrite.mjs
+++ temp.rewrite.mjs
@@ -30,39 +30,39 @@ enter_rule ("item_default");
     set_return (`${c.rwr ()}`);
 return exit_rule ("item_default");
 },
-geometry : function (_geo,ws1,_colon,_ws5,lb,ws2,x,y,w,h,as,rect,rb,ws3,_comma,ws4,) {
+geometry : function (_geo,ws1,_colon,ws5,lb,ws2,x,y,w,h,as,rect,rb,ws3,_comma,ws4,) {
 enter_rule ("geometry");
-    set_return (`\n${_geo.rwr ()}${ws1.rwr ()}${_colon.rwr ()}${_ws5.rwr ()}${lb.rwr ()}${ws2.rwr ()}${x.rwr ()}${y.rwr ()}${w.rwr ()}${h.rwr ()}${as.rwr ().join ('')}${rect.rwr ().join ('')}${rb.rwr ()}${ws3.rwr ()}${_comma.rwr ()}${ws4.rwr ()}`);
+    set_return (`\n"boundingbox" : [${x.rwr ()},${y.rwr ()},${w.rwr ()},${h.rwr ()}],`);
 return exit_rule ("geometry");
 },
 x : function (_,ws1,_colon,ws2,n,_comma,ws3,) {
 enter_rule ("x");
-    set_return (`${_.rwr ()}${ws1.rwr ()}${_colon.rwr ()}${ws2.rwr ()}${n.rwr ()}${_comma.rwr ()}${ws3.rwr ()}`);
+    set_return (`${n.rwr ()}`);
 return exit_rule ("x");
 },
 y : function (_,ws1,_colon,ws2,n,_comma,ws3,) {
 enter_rule ("y");
-    set_return (`${_.rwr ()}${ws1.rwr ()}${_colon.rwr ()}${ws2.rwr ()}${n.rwr ()}${_comma.rwr ()}${ws3.rwr ()}`);
+    set_return (`${n.rwr ()}`);
 return exit_rule ("y");
 },
 width : function (_,ws1,_colon,ws2,n,_comma,ws3,) {
 enter_rule ("width");
-    set_return (`${_.rwr ()}${ws1.rwr ()}${_colon.rwr ()}${ws2.rwr ()}${n.rwr ()}${_comma.rwr ()}${ws3.rwr ()}`);
+    set_return (`${n.rwr ()}`);
 return exit_rule ("width");
 },
 height : function (_,ws1,_colon,ws2,n,_comma,ws3,) {
 enter_rule ("height");
-    set_return (`${_.rwr ()}${ws1.rwr ()}${_colon.rwr ()}${ws2.rwr ()}${n.rwr ()}${_comma.rwr ()}${ws3.rwr ()}`);
+    set_return (`${n.rwr ()}`);
 return exit_rule ("height");
 },
 as : function (_,ws1,_colon,ws2,s,_comma,ws3,) {
 enter_rule ("as");
-    set_return (`${_.rwr ()}${ws1.rwr ()}${_colon.rwr ()}${ws2.rwr ()}${s.rwr ()}${_comma.rwr ()}${ws3.rwr ()}`);
+    set_return (``);
 return exit_rule ("as");
 },
 rectangle : function (_,ws1,_colon,ws2,lb,ws3,cs,rb,ws4,) {
 enter_rule ("rectangle");
-    set_return (`${_.rwr ()}${ws1.rwr ()}${_colon.rwr ()}${ws2.rwr ()}${lb.rwr ()}${ws3.rwr ()}${cs.rwr ().join ('')}${rb.rwr ()}${ws4.rwr ()}`);
+    set_return (``);
 return exit_rule ("rectangle");
 },
 string : function (ldq,cs,rdq,ws,) {
