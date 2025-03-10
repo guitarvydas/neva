01762ff159138003b0d87ab3b7a244d6f83a7c8e
Paul Tarvydas <paul.tarvydas@gmail.com>
2025-03-10 07:26:39 -0400

    add ":" ws5 to geometry

--- temp.nanodsl.mjs
+++ temp.nanodsl.mjs
@@ -40,7 +40,7 @@ geometry {
   item =
     | geometry -- geometry
     | any      -- default
-  geometry = str<"geometry"> ws "{" ws x y width height as? rectangle? "}" ws "," ws
+  geometry = str<"geometry"> ws ":" ws "{" ws x y width height as? rectangle? "}" ws "," ws
   x = str<"x"> ws ":" ws number "," ws
   y = str<"y"> ws ":" ws number "," ws
   width = str<"width"> ws ":" ws number "," ws
@@ -102,39 +102,39 @@ enter_rule ("item_default");
     set_return (`${c.rwr ()}`);
 return exit_rule ("item_default");
 },
-geometry : function (_geo,ws1,lb,ws2,x,y,w,h,as,rect,rb,ws3,_comma,ws4,) {
+geometry : function (_geo,ws1,_colon,_ws5,lb,ws2,x,y,w,h,as,rect,rb,ws3,_comma,ws4,) {
 enter_rule ("geometry");
-    set_return (`\n${_geo.rwr ()}${ws1.rwr ()}${lb.rwr ()}${ws2.rwr ()}${x.rwr ()}${y.rwr ()}${w.rwr ()}${h.rwr ()}${as.rwr ().join ('')}${rect.rwr ().join ('')}${rb.rwr ()}${ws3.rwr ()}${_comma.rwr ()}${ws4.rwr ()}`);
+    set_return (`\n${_geo.rwr ()}${ws1.rwr ()}${_colon.rwr ()}${_ws5.rwr ()}${lb.rwr ()}${ws2.rwr ()}${x.rwr ()}${y.rwr ()}${w.rwr ()}${h.rwr ()}${as.rwr ().join ('')}${rect.rwr ().join ('')}${rb.rwr ()}${ws3.rwr ()}${_comma.rwr ()}${ws4.rwr ()}`);
 return exit_rule ("geometry");
 },
 x : function (_,ws1,_colon,ws2,n,_comma,ws3,) {
 enter_rule ("x");
-    set_return (`${n.rwr ()}`);
+    set_return (`${_.rwr ()}${ws1.rwr ()}${_colon.rwr ()}${ws2.rwr ()}${n.rwr ()}${_comma.rwr ()}${ws3.rwr ()}`);
 return exit_rule ("x");
 },
 y : function (_,ws1,_colon,ws2,n,_comma,ws3,) {
 enter_rule ("y");
-    set_return (`${n.rwr ()}`);
+    set_return (`${_.rwr ()}${ws1.rwr ()}${_colon.rwr ()}${ws2.rwr ()}${n.rwr ()}${_comma.rwr ()}${ws3.rwr ()}`);
 return exit_rule ("y");
 },
 width : function (_,ws1,_colon,ws2,n,_comma,ws3,) {
 enter_rule ("width");
-    set_return (`${n.rwr ()}`);
+    set_return (`${_.rwr ()}${ws1.rwr ()}${_colon.rwr ()}${ws2.rwr ()}${n.rwr ()}${_comma.rwr ()}${ws3.rwr ()}`);
 return exit_rule ("width");
 },
 height : function (_,ws1,_colon,ws2,n,_comma,ws3,) {
 enter_rule ("height");
-    set_return (`${n.rwr ()}`);
+    set_return (`${_.rwr ()}${ws1.rwr ()}${_colon.rwr ()}${ws2.rwr ()}${n.rwr ()}${_comma.rwr ()}${ws3.rwr ()}`);
 return exit_rule ("height");
 },
 as : function (_,ws1,_colon,ws2,s,_comma,ws3,) {
 enter_rule ("as");
-    set_return (``);
+    set_return (`${_.rwr ()}${ws1.rwr ()}${_colon.rwr ()}${ws2.rwr ()}${s.rwr ()}${_comma.rwr ()}${ws3.rwr ()}`);
 return exit_rule ("as");
 },
 rectangle : function (_,ws1,_colon,ws2,lb,ws3,cs,rb,ws4,) {
 enter_rule ("rectangle");
-    set_return (``);
+    set_return (`${_.rwr ()}${ws1.rwr ()}${_colon.rwr ()}${ws2.rwr ()}${lb.rwr ()}${ws3.rwr ()}${cs.rwr ().join ('')}${rb.rwr ()}${ws4.rwr ()}`);
 return exit_rule ("rectangle");
 },
 string : function (ldq,cs,rdq,ws,) {
